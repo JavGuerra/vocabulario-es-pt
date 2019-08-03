742b782b682e23b1222b09f3cb8d7e45c1266813
Javier Guerra <javguerra@badared.com>
2019-08-03 19:03:45 +0200

    v.0.1

---
+++ diccionario.js
@@ -0,0 +1,106 @@
+/*
+ * Diccionario de frases español y portugués
+ * -----------------------------------------
+ * 2019-08-03 v.0.1 javguerra
+ *
+ * Escriba sus frases de esta forma entre los dos corchetes ({}) de diccionario:
+ * "frase en español" entre comillas, dos puntos (:), "frase en portugués" entre comillas,
+ * seguido de una coma (,) final, y luego pase a otra línea.
+ * En la última línea de la lista no ponga coma.
+ *
+ * Las frases no necesitan seguir un orden alfabético o similar.
+ *
+ * Importante: No introduzca frases repetidas o de respuestas multiples.
+ *
+ * Se emplea el masculino por defecto en las traducciones.
+ * 
+ * Selección de frases tomadas de:
+ * https://elblogdeidiomas.es/frases-y-vocabulario-en-portugues-para-viajeros/
+ */
+ 
+var diccionario = {
+
+    "Hola": "Olá",
+    "¿Cómo estás?": "Como esta?",
+    "¿Qué tal?": "Tudo bem?",
+    "Estoy bien": "Estou bem",
+    "Bien, gracias ¿y tú?": "Estou bem, obrigado, e você?",
+    "Buenos días": "Bom dia",
+    "Buenas tardes": "Boa tarde",
+    "Buenas noches": "Boa noite",
+    "Adiós": "Adeus",
+    "Hasta luego": "Até logo",
+    "Hasta mañana": "Até",
+    "Hasta la próxima": "Até outra vez",
+    "Gracias": "Obrigado",
+    "Muchas gracias": "Muito obrigado",
+    "De nada": "De nada",
+    "No hay de qué": "Não há de quê",
+    "Permiso": "Com licença",
+    "Disculpe": "Desculpe",
+    "Por favor": "Por favor",
+    "No se preocupe": "Não tem problema",
+    "No entiendo": "Não entendo",
+    "No hablo mucho portugués": "Não falo muito portugués",
+    "No sé": "Não sei",
+    "¿Puedes repetir?": "Pode repetir?",
+    "Sí": "Sim",
+    "No": "Não",
+    "¿Puede hablar más despacio?": "Pode falar mais devagar?",
+    "¿Cómo puedo llegar a...?": "Como posso chegar até...",
+    "Me gustaría ir...": " Eu gostaria de ir...",
+    "Centro ciudad": "Centro da cidade",
+    "¿Está cerca?": "Fica perto?",
+    "¿Está lejos?": "Fica longe?",
+    "Todo recto": "Direto",
+    "Izquierda": "Esquerda",
+    "Derecha": "Direita",
+    "¿Dónde está el baño?": "Onde é o banheiro?",
+    "Estoy perdido": "Estou perdido",
+    "Hotel": "Hotel",
+    "¿Puedes ayudarme?": "Você pode me ajudar?",
+    "¿Cuál es el nombre de esta calle?": "Qual é o nome dessa rua?",
+    "¿Cuál es la dirección?": "¿Cuál é o endereço?",
+    "¿Cómo hago para llegar allí?": "Como faço para chegar lá?",
+    "Parada de autobús": "Parada de ônibus",
+    "Estación de metro": "Estação de metrô",
+    "Coger un taxi": "Pegar um taxi",
+    "¿Cómo puedo ir a la estación de tren?": "Como posso ir para a estação de trem?",
+    "¿Cómo puedo ir al aeropuerto?": "Como posso ir para o aeroporto?",
+    "¿Cuánto cuesta?": "Quanto custa?",
+    "Es barato": "É barato",
+    "Es muy barato": "É muito barato",
+    "Es caro": "É caro",
+    "Es demasiado caro": "É muito caro",
+    "Me encanta": "Eu gosto",
+    "Lo odio": "Eu odeio",
+    "Solo estoy mirando": "Estou apenas olhando",
+    "Me gustaría comprar este": "Gostaria de comprar este",
+    "Mi talla es...": "Meu tamaño é...",
+    "Quiero comprar...": "Quería comprar...",
+    "Estoy buscando...": "Estou procurando...",
+    "¿Tiene probador?": "Tem provador?",
+    "Estaba delicioso": "Estava delicioso",
+    "¿Qué me recomienda?": "O que você me aconselha?",
+    "¿Cuál es el mejor restaurante de la ciudad?": "Qual é o melhor restaurante da cidade?",
+    "¿Podría traerme el menú?": "Poderia trazer o menu?",
+    "Una mesa para dos personas, por favor": "Uma mesa para duas pesoas, por favor",
+    "Quiero cenar...": "Eu quero jantar...",
+    "Para beber": "Para beber",
+    "cerveza": "cerveja",
+    "vino": "vinho",
+    "La cuenta, por favor": "A conta, por favor",
+    "almuerzo": "almoço",
+    "postre": "sobremesa",
+    "Camarero": "Garçom",
+    "Que aproveche": "Bom apetite",
+    "¡Salud!": "Saúde!",
+    "Me gustaría...": "Eu gostaria...",
+    "Yo voy a querer...": "Eu vou querer...",
+    "Quiero ir al aeropuerto, por favor": "Quero ir ao aeroporto, por favor",
+    "¿Cuándo llega el vuelo?": "Quando chega o vôo?",
+    "¿Cuándo sale el vuelo?": "Quando sai o vôo?",
+    "¿Dónde puedo pesar mi equipaje?": "Onde posso pesa minha bagagem?",
+    "¿Cuál es mi número de vuelo?": "Qual é o meu número de vôo?"
+
+};
---
+++ js/script.js
@@ -0,0 +1,135 @@
+$(document).ready(function () {
+
+    // Variables
+
+    // conmuta si false = español al inicio
+    // conmuta si true = portugués al inicio
+    var conmuta = false;
+    var respuesta = ""; // contiene la respuesta recibida
+    var claves = []; // contiene las claves del diccionario
+    // var clave contiene la clave aleatoria
+    var valor = ""; // contiene el valor de la clave
+    // var diccionario contiene el diccionario de palabras
+    var idioma = { // contiene las traducciones de la interfaz
+        "Vocabulario": "Vocabulário",
+        "Escriba en portugués:": "Escreva em espanhol:",
+        "Comprobar": "Cheque",
+        "Cambiar para traducir del portugués al español": "Alterar para traduzir do espanhol para o português",
+        "Aceptar": "Aceitar"
+    };
+    var aleatorio = 0; // contiene el número aleatorio
+    var ld = 0; // contiene la longitud de diccionario
+    var i = 1; // para iteraciones y apoyo
+
+
+    // Inicio
+
+    // Obtiene array de las claves del diccionario
+    for (var clave in diccionario) {
+        if (diccionario.hasOwnProperty(clave)) { //comprueba si tiene clave
+            claves.push(clave);
+        }
+    }
+    ld = claves.length;
+    // Inicia el texto de la interfaz
+    ponerTexto();
+
+
+    // Eventos
+
+    // Al pulsar el botón "Comprobar"...
+    $('#txt3').click(function (e) {
+        event.preventDefault(); // No ejecutes el botón
+        this.disabled = true;
+        comprobar();
+    });
+
+    // Al pulsar el checkbox...
+    $('#conmutador').click(function (e) {
+        conmuta = !conmuta; // swich
+        ponerTexto();
+    });
+
+    // Al abrir la ventana, pone el foco en el botón
+    $('#ventana').on('shown.bs.modal', function (e) {
+        $('#txt5').focus();
+    });
+
+    // Al cerrar la ventana, limpia y pone el foco en el cuadro
+    $('#ventana').on('hidden.bs.modal', function (e) {
+        $("#traduccion").val('').focus();
+        $("#clave").empty();
+        $("#respuesta").empty();
+        $("#valor").empty();
+        $('#txt3').prop("disabled", false);
+    });
+
+
+    // Funciones
+
+    // Comprueba si la respuesta dada es correcta
+    function comprobar() {
+        respuesta = $("#traduccion").val();
+        if (respuesta == null || respuesta.length == 0 || /^\s*$/.test(respuesta)) {
+            if (!conmuta) {
+                i = "No hay nada escrito";
+            } else {
+                i = "Não há nada escrito";
+            }
+            $("#respuesta").text(i);
+            $("#icono").attr("src", "img/warning.svg");
+            $("#ventana").modal('show');
+            $("#txt5").focus();
+        } else {
+            respuesta = respuesta.trim();
+            if (respuesta == valor) {
+                $("#icono").attr("src", "img/check.svg");
+            } else {
+                if (!conmuta) {
+                    i = "Debió decir: ";
+                } else {
+                    i = "Deveria ter dito: ";
+                }
+                $("#icono").attr("src", "img/circle-x.svg");
+                $("#valor").html(i + "<b>" + valor + "</b>");
+            }
+            $("#clave").text(clave);
+            $("#respuesta").text(respuesta);
+            $("#ventana").modal('show');
+            nuevaTraduccion();
+        }
+    }
+
+
+    // Pone el texto de la interfaz en el idioma correcto
+    function ponerTexto() {
+        i = 1;
+        if (!conmuta) {
+            for (var clave in idioma) {
+                $("#txt" + i).text(clave);
+                i++;
+            }
+        } else {
+            for (var clave in idioma) {
+                $("#txt" + i).text(idioma[clave]);
+                i++;
+            }
+        }
+        nuevaTraduccion();
+    }
+
+
+    // Obtiene y pone una nueva traducción
+    function nuevaTraduccion() {
+        // Obtiene un número entre 0 y la longitud del diccionario menos 1
+        aleatorio = Math.floor(Math.random() * ld);
+        clave = claves[aleatorio];
+        valor = diccionario[clave];
+        if (conmuta) {
+            valor = [clave, clave = valor][0]; // intercambia las variables clave y valor
+        }
+        $("#traduccion").val('').focus();
+        $("#palabra").text(clave).hide().fadeIn(300);
+    }
+
+});
